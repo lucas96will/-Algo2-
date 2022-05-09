5f4dd90712f9e870370ca214af9f12c2ee6fd5bc
Lucas Williams <lucas-w147@hotmail.com>
2022-05-09 15:55:19 -0300

    Pequeños fix

--- abb.c
+++ abb.c
@@ -1,56 +1,50 @@
-#ifndef _ALGO2__ABB_H
-#define _ALGO2__ABB_H
+#include "abb.h"
+#include <stdlib.h>
+#include <stdbool.h>
 
-typedef struct abb abb_t;
+typedef struct nodo nodo_t;
 
-typedef int (*abb_comparar_clave_t) (const char *, const char *);
-typedef void (*abb_destruir_dato_t) (void *);
+struct nodo {
+    nodo_t* derecha;
+    nodo_t* izquierda;
+    void* dato;
+    void* clave;
+};
+
+struct abb {
+    nodo_t* raiz;
+};
+
+
+
+/* *****************************************************************
+ *                    FUNCIONES AUXILIARES DEL ABB
+ * *****************************************************************/
+
+
+
 
 /* *****************************************************************
  *                    PRIMITIVAS DEL ABB
  * *****************************************************************/
 
-/*
- * Pre:
- * Post:
- * */
+
 abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato);
 
-/*
- * Pre:
- * Post:
- * */
+
 bool abb_guardar(abb_t *arbol, const char *clave, void *dato);
 
-/*
- * Pre:
- * Post:
- * */
+
 void *abb_borrar(abb_t *arbol, const char *clave);
 
-/*
- * Pre:
- * Post:
- * */
+
 void *abb_obtener(const abb_t *arbol, const char *clave);
 
-/*
- * Pre:
- * Post:
- * */
+
 bool abb_pertenece(const abb_t *arbol, const char *clave);
 
-/*
- * Pre:
- * Post:
- * */
+
 size_t abb_cantidad(const abb_t *arbol);
 
-/*
- * Pre:
- * Post:
- * */
+
 void abb_destruir(abb_t *arbol);
-
-
-#endif //_ALGO2__ABB_H
