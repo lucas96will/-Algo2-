7b005708d2228f04901540b5ad6ec0a32f1a4f56
Lucas Williams <lucas-w147@hotmail.com>
2022-05-08 23:06:10 -0300

    Pequeños fix

--- hash.c
+++ hash.c
@@ -1,7 +1,7 @@
 #include "hash.h"
-#define _POSIX_C_SOURCE 200809L
 #include <stdlib.h>
 #include <stdio.h>
+#define _POSIX_C_SOURCE 200809L
 #include <string.h>
 
 #define CAPACIDAD_INICIAL 101 // 276 excelente, 292 muy bueno, 301 bueno
