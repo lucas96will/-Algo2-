3cd5443cc52c6b51b9b45c255650b6177cdfa235
Lucas Williams <lucas-w147@hotmail.com>
2022-05-27 12:03:23 -0300

    archivos base

--- heap.c
+++ heap.c
@@ -1,22 +1,75 @@
 #include "heap.h"
 
-#define RATIO 0.6
-#define RESIZE 2
+#define FACTOR_REDIMENSION 2
+#define TAM_INICIAL 20
+#define MULTIPLO_CANTIDAD 4
 
 typedef struct heap {
-    void** arreglo;
-    size_t cant;
-    size_t tam;
-    cmp_func_t* cmp;
+    void** datos;
+    size_t cantidad;
+    size_t capacidad;
+    cmp_func_t cmp;
 } heap_t;
 
+/* ******************************************************************
+ *            DEFINICION FUNCIONES AUXILIARES PARA HEAP
+ * *****************************************************************/
+
+/*
+ * Pre:
+ * Post:
+ */
+bool down_heap(void* arreglo[], size_t tam, cmp_func_t cmp, size_t pos);
+
+/*
+ * Pre:
+ * Post:
+ */
+bool up_heap(void* arreglo[], size_t tam, cmp_func_t cmp, size_t pos);
+
+/*
+ * Pre:
+ * Post:
+ */
+void heapify(void* arreglo[], size_t tam, cmp_func_t cmp);
+
+/*
+ * Pre: el heap fue creado y nueva_capacidad > 0
+ * Post: hace realloc del arreglo heap->datos con la nueva capacidad
+ * Devuelve true si el realloc fue exitoso y nueva_capacidad > 0 o false
+ * en otro caso
+ */
+bool heap_redimensionar(heap_t *heap, size_t nueva_capacidad);
 
 
 /* ******************************************************************
- *                FUNCIONES AUXILIARES PARA HEAP
+ *            IMPLEMENTACION FUNCIONES AUXILIARES PARA HEAP
  * *****************************************************************/
 
+bool down_heap(void* arreglo[], size_t tam, cmp_func_t cmp, size_t pos) {
 
+}
+
+
+bool up_heap(void* arreglo[], size_t tam, cmp_func_t cmp, size_t pos) {
+
+}
+
+void heapify(void* arreglo[], size_t tam, cmp_func_t cmp) {
+
+}
+
+bool heap_redimensionar(heap_t *heap, size_t nueva_capacidad) {
+    void** datos_nuevo = realloc(heap->datos, nueva_capacidad * sizeof(void*));
+
+    if(nueva_capacidad > 0 && datos_nuevo == NULL){
+        return false;
+    }
+
+    heap->datos = datos_nuevo;
+    heap->capacidad = nueva_capacidad;
+    return true;
+}
 
 
 /* ******************************************************************
@@ -27,7 +80,105 @@ typedef struct heap {
 heap_t *heap_crear(cmp_func_t cmp) {
     if(!cmp) return NULL;
 
+    heap_t* heap = malloc(sizeof(heap_t));
+    if(!heap) return NULL;
 
+    heap->datos = malloc(sizeof(void*) * TAM_INICIAL);
+    if(!heap->datos){
+        free(heap);
+        return NULL;
+    }
+
+    heap->cantidad = 0;
+    heap->capacidad = TAM_INICIAL;
+    heap->cmp = cmp;
 
     return heap;
+}
+
+heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp) {
+    if(!cmp) return NULL;
+
+    heap_t* heap = malloc(sizeof(heap_t));
+    if(!heap) return NULL;
+
+    heap->datos = malloc(sizeof(void*) * (n * FACTOR_REDIMENSION));
+    if(!heap->datos) {
+        free(heap);
+        return NULL;
+    }
+    heap->capacidad = n * FACTOR_REDIMENSION;
+    heap->cantidad = n;
+    heap->cmp = cmp;
+
+    for(int i = 0; i < n; i++) {
+        heap->datos[i] = arreglo[i];
+    }
+
+    heapify(heap->datos, heap->cantidad, cmp);
+
+    return heap;
+}
+
+void heap_destruir(heap_t *heap, void destruir_elemento(void *e)) {
+    if(destruir_elemento != NULL) {
+        for(int i = 0; i < heap->cantidad; i++) {
+            destruir_elemento(heap->datos[i]);
+        }
+    }
+    free(heap->datos);
+    free(heap);
+}
+
+size_t heap_cantidad(const heap_t *heap) {
+    return heap->cantidad;
+}
+
+bool heap_esta_vacio(const heap_t *heap) {
+    return (heap_cantidad(heap) > 0);
+}
+
+bool heap_encolar(heap_t *heap, void *elem) {
+
+    // Agrego al final
+    heap->datos[heap->cantidad] = elem;
+    heap->cantidad++;
+
+    if(heap->cantidad == heap->capacidad) {
+        if(!heap_redimensionar(heap, FACTOR_REDIMENSION * heap->capacidad)) {
+            return false;
+        }
+    }
+
+    // Up_heap del ultimo elemento (cantidad - 1)
+    up_heap(heap->datos, heap->cantidad, heap->cmp, heap->cantidad-1);
+
+    return true;
+}
+
+void *heap_ver_max(const heap_t *heap) {
+    if(heap_esta_vacio(heap)) {
+        return NULL;
+    }
+    return heap->datos[0];
+}
+
+void *heap_desencolar(heap_t *heap) {
+    void* dato = heap->datos[0];
+    heap->datos[0] = heap->datos[heap->cantidad];
+    heap->datos[heap->cantidad] = NULL;
+
+    // Disminuyo en 1 la cantidad
+    heap->cantidad--;
+
+    // Hago downheap del primer elemento
+    down_heap(heap->datos, heap->cantidad, heap->cmp, 0);
+
+    // Redimensiono si es el caso
+
+    if(MULTIPLO_CANTIDAD*heap->cantidad <= heap->capacidad && heap->cantidad != 0) {
+        heap_redimensionar(heap, heap->capacidad / FACTOR_REDIMENSION);
+    }
+
+    return dato;
 }
--- heap.h
+++ heap.h
@@ -24,7 +24,7 @@ typedef int (*cmp_func_t) (const void *a, const void *b);
  */
 heap_t *heap_crear(cmp_func_t cmp);
 
-/* Se crea un heap a partir de un arreglo y una funcion de comparacion
+/* Se crea un heap a partir de un datos y una funcion de comparacion
  * Pre: La función de comparación (de tipo cmp_func_t) debe ser correcta
  * Post: Se devuelve el heap creado, si no se pudo crear devuelve NULL
  */
@@ -57,8 +57,7 @@ bool heap_esta_vacio(const heap_t *heap);
  */
 bool heap_encolar(heap_t *heap, void *elem);
 
-/* Se devuelve el elemento de forma const, de manera que no se pueda editar
- * y romper la estructura interna del heap
+/*
  * Pre: El heap fue creado
  * Post: Se devuelve el primer elemento del heap, si no se pudo devuelve NULL
  */
@@ -73,7 +72,7 @@ void *heap_desencolar(heap_t *heap);
 
 /*
  * Pre: La funcion de comparacion es valida
- * Post: el arreglo elementos[] queda ordenado de manera
+ * Post: el datos elementos[] queda ordenado de manera
  * usando la funcion de comparacion de cualquier elemento y su
  * siguiente, siga la validez de la misma
  */
