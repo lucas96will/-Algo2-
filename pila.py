923a62ef0d1a1d1ef43595db854aff57296af197
Lucas Williams <lucas-w147@hotmail.com>
2022-06-30 11:15:18 -0300

    PEP

--- pila.py
+++ pila.py
@@ -1,24 +1,24 @@
 class Pila:
     def __init__(self):
-        '''
+        """
         Inicializa una nueva pila, vacía
-        '''
+        """
         self.tope = None
 
     def apilar(self, dato):
-        '''
+        """
         Agrega un nuevo elemento a la pila
-        '''
+        """
         nodo = _Nodo(dato, self.tope)
         self.tope = nodo
 
     def desapilar(self):
-        '''
+        """
         Desapila el elemento que está en el tope de la pila
         y lo devuelve.
         Pre: la pila NO está vacía.
         Pos: el nuevo tope es el que estaba abajo del tope anterior
-        '''
+        """
         if self.esta_vacia():
             raise ValueError("pila vacía")
         dato = self.tope.dato
@@ -26,19 +26,20 @@ class Pila:
         return dato
 
     def ver_tope(self):
-        '''
+        """
         Devuelve el elemento que está en el tope de la pila.
         Pre: la pila NO está vacía.
-        '''
+        """
         if self.esta_vacia():
             raise ValueError("pila vacía")
         return self.tope.dato
 
     def esta_vacia(self):
-        '''
+        """
         Devuelve True o False según si la pila está vacía o no
-        '''
+        """
         return self.tope is None
+
 
 class _Nodo:
     def __init__(self, dato, prox=None):
