7bedd8e5bbc4b01dbae277b7be87492ee680fe3c
Lucas Williams <lucas-w147@hotmail.com>
2022-07-06 18:05:55 -0300

    cambio: las canciones favoritas del usuario no se toman en cuenta en la lista de recomendados

--- entrega.mk
+++ entrega.mk
@@ -1,3 +1,3 @@
-recomendify: recomendify.py biblioteca_recomendify.py grafo.py grafo_funciones.py
+recomendify: main.py recomendify.py biblioteca_recomendify.py grafo.py grafo_funciones.py
     cp recomendify.py recomendify
     chmod +x recomendify
