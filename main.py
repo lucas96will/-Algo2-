75c076c1f197b1ce440ffea56151c859416f1d71
Lucas Williams <lucas-w147@hotmail.com>
2022-07-06 19:10:11 -0300

    pep

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 from recomendify import Recomendify
 import sys
+import random
 
 ARG_ARCHIVO_PROCESADO = 1
+
+
 def main(argv):
     if len(argv) != 2:
         return
     recomendify = Recomendify(argv[ARG_ARCHIVO_PROCESADO])
+
     recomendify.iniciar_recomendify()
 
 
